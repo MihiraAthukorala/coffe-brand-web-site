a9196567ab7e428db6fd619f054db78d533d6775
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-05-29 11:31:33 +0530

    home section cunfigure today

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Menu, Search, Cart Button Activating
 
-// Menu Button Activatig
+// Menu Button Activating
 
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
 }
 
 // Search Button Activating
@@ -14,6 +16,8 @@ let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
 }
 
 // Cart Button Activating
@@ -22,4 +26,14 @@ let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#cart-btn').onclick = () => {
     cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+// Configue Button Items
+ 
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
 }
